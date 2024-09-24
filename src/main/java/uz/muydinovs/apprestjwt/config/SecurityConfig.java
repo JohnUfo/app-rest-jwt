2cd49eb787ba7be356a209e002d3c41b8a317333
johnUfo <muydinovumidjon17@gmail.com>
2024-09-24 15:31:13 +0500

    authentication manager is added and it is working

--- src/main/java/uz/muydinovs/apprestjwt/config/SecurityConfig.java
+++ src/main/java/uz/muydinovs/apprestjwt/config/SecurityConfig.java
@@ -3,6 +3,9 @@ package uz.muydinovs.apprestjwt.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -49,5 +52,13 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService());
+        return authenticationManagerBuilder.build();
     }
 }
--- src/main/java/uz/muydinovs/apprestjwt/controller/AuthController.java
+++ src/main/java/uz/muydinovs/apprestjwt/controller/AuthController.java
@@ -3,37 +3,35 @@ package uz.muydinovs.apprestjwt.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uz.muydinovs.apprestjwt.payload.LoginDto;
 import uz.muydinovs.apprestjwt.security.JwtProvider;
-import uz.muydinovs.apprestjwt.service.MyAuthService;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
     @Autowired
-    MyAuthService myAuthService;
-
-    @Autowired
     JwtProvider jwtProvider;
 
     @Autowired
-    PasswordEncoder passwordEncoder;
+    AuthenticationManager authenticationManager;
 
     @PostMapping("/login")
     public HttpEntity<?> loginToSystem(@RequestBody LoginDto loginDto) {
-        UserDetails userDetails = myAuthService.loadUserByUsername(loginDto.getUsername());
-        boolean existUser = passwordEncoder.matches(loginDto.getPassword(), userDetails.getPassword());
-        if (existUser) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword()));
             String token = jwtProvider.generateToken(loginDto.getUsername());
             return ResponseEntity.ok(token);
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.status(401).body("Username or password is incorrect");
         }
-        return ResponseEntity.status(401).body("Username or password is incorrect");
+
     }
 }
